c08f14d7786891099cc2fa7d3197ca68324d6e15
Thomas Anciaux <thomas.anciaux@miaaguard.com>
2018-01-15 20:50:39 +0100

    Initial navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, RouterLink } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { OfferComponent } from './offer/offer.component';
+import { SearchComponent } from './search/search.component';
 
-
+const ROUTES = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'offer',
+    component: OfferComponent
+  },
+  {
+    path: 'search',
+    component: SearchComponent
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OfferComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
