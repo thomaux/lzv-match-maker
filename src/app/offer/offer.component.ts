51eb8be5909cd27c5a62186c692a35be7d158503
Thomas Anciaux <thomas.anciaux@miaaguard.com>
2018-01-29 21:32:29 +0100

    Sync offer form with model

--- src/app/offer/offer.component.ts
+++ src/app/offer/offer.component.ts
@@ -9,7 +9,7 @@ import { Offer } from './offer';
 })
 export class OfferComponent implements OnInit {
 
-  model = new Offer(undefined, undefined, [], true);
+  model = new Offer(undefined, { postalCode: undefined, gymName: undefined }, [], true);
 
   constructor(private offerService: OfferService) { }
 
@@ -19,5 +19,18 @@ export class OfferComponent implements OnInit {
   onSubmit() {
     this.offerService.createOffer(this.model)
       .then(() => alert('success'))
+  }
+
+  isLevelSelected(lvl: number) {
+    return this.model.forLevels.indexOf(lvl) > -1;
+  }
+
+  toggleLevelSelection(lvl: number) {
+    const idx = this.model.forLevels.indexOf(lvl);
+    if(idx > -1) {
+      this.model.forLevels.splice(idx, 1);
+    } else {
+      this.model.forLevels.push(lvl);
+    }
   }
 }
