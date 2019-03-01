da69d68bf0628c84c4fb88b3ef5bba32ffc42628
Thomas Anciaux <thomas.anciaux@miaaguard.com>
2019-03-01 17:48:14 +0100

    Set cookie lifetime of 1y

--- src/server/index.ts
+++ src/server/index.ts
@@ -34,7 +34,7 @@ app.use(session({
     secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: true },
+    cookie: { secure: true, maxAge: 365 * 24 * 60 * 60 * 1000 },
     store: new MongoStore({
         mongooseConnection: connection
     })
