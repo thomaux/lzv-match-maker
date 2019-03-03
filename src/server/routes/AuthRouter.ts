6ee73c142e1da42204e60bf2e35d13fe7e61ce60
Thomas Anciaux <thomas.anciaux@miaaguard.com>
2019-03-03 12:53:19 +0100

    Implements logout

--- src/server/routes/AuthRouter.ts
+++ src/server/routes/AuthRouter.ts
@@ -3,5 +3,15 @@ import * as passport from 'passport';
 
 export const authRouter = Router();
 
-authRouter.get("/", passport.authenticate('facebook'));
-authRouter.get("/callback", passport.authenticate('facebook'));
+authRouter.route('/')
+    .get(passport.authenticate('facebook'))
+    .delete((req, res, next) => {
+        req.logout();
+        req.session.destroy(err => {
+            if (err) {
+                return next(err);
+            }
+            res.status(204).end();
+        });
+    });
+authRouter.get('/callback', passport.authenticate('facebook'));
