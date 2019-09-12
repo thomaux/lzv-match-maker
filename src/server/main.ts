a552df498e9f61ef828d1f24a8738cd42a0a433c
Thomas Anciaux <thomas.anciaux@miaaguard.com>
2019-09-12 11:43:53 +0200

    Support HTTPS

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as fs from 'fs';
 import * as passport from 'passport';
 import { AppModule } from './modules/app/AppModule';
 
+const httpsOptions = {
+    key: fs.readFileSync('cert/localhost.key', 'utf8'),
+    cert: fs.readFileSync('cert/localhost.crt', 'utf8')
+};
+
 async function bootstrap() {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+        httpsOptions
+    });
 
     app.use(passport.initialize());
     app.use(passport.session());
