369d157a3c376ff94f6a08fbef32652b12144e92
Thomas Anciaux <thomas.anciaux@miaaguard.com>
2019-09-19 00:33:55 +0200

    Added static serving and implemented logout

--- src/server/modules/auth/AuthController.ts
+++ src/server/modules/auth/AuthController.ts
@@ -1,5 +1,5 @@
-import { Controller, Delete, Get, Req, UseGuards, Next, HttpCode } from '@nestjs/common';
-import { Request, NextFunction } from 'express';
+import { Controller, Delete, Get, Next, Req, Res, UseGuards } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
 import { FacebookGuard } from './FacebookGuard';
 
 @Controller('auth')
@@ -7,18 +7,16 @@ export class AuthController {
 
     @Get()
     @UseGuards(FacebookGuard)
-    login() {
-
-    }
+    login() {}
 
     @Delete()
-    @HttpCode(204)
-    logout(@Req() req: Request, @Next() next: NextFunction) {
+    logout(@Req() req: Request, @Res() res: Response, @Next() next: NextFunction) {
         req.logout();
         req.session.destroy(err => {
             if (err) {
                 return next(err);
             }
+            res.status(204).end();
         });
     }
 
