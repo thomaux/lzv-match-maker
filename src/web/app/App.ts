882bdd64730c90ac2c5d1fd347e1c4079b9c190a
Thomas Anciaux <thomas.anciaux@miaaguard.com>
2019-09-29 13:57:23 +0200

    Set up web app with Vue, TypeScript & Parcel

---
+++ src/web/app/App.ts
@@ -0,0 +1,9 @@
+import { Component, Vue } from 'vue-property-decorator';
+import template from './App.html';
+
+@Component({
+    template
+})
+export class App extends Vue {
+
+}
---
+++ src/web/main.ts
@@ -0,0 +1,11 @@
+import { App } from './app/App';
+import Vue from 'vue';
+
+// tslint:disable-next-line: no-unused-expression
+new Vue({
+    el: '#app',
+    template: '<app></app>',
+    components: {
+        App
+    }
+});
