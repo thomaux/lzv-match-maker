7d20d5c8bab6b8199d6d34614ed4ba27d1b1b0cb
Thomas Anciaux <1404536+thomaux@users.noreply.github.com>
2020-03-27 13:22:54 +0100

    Include Team  in AppModule

--- src/modules/app/AppModule.ts
+++ src/modules/app/AppModule.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from '../auth/AuthModule';
 import { ListingModule } from '../listing/ListingModule';
 import { LocationModule } from '../location/LocationModule';
+import { TeamModule } from '../team/TeamModule';
 
 @Module({
     imports: [
         MongooseModule.forRoot(`mongodb://${process.env.MONGO_USER}:${encodeURIComponent(process.env.MONGO_SECRET)}@${process.env.MONGO_HOST}`, { useNewUrlParser: true, useUnifiedTopology: true }),
         LocationModule,
         ListingModule,
-        AuthModule]
+        AuthModule,
+        TeamModule]
 })
 export class AppModule { }
